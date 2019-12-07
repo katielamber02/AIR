ba8e227d5b4a9be0004ec93a71d92ca66717aa8a
Katie Lamber <katielamber02@gmail.com>
2019-12-07 16:57:52 +0200

    submit implemented

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,23 +1,18 @@
 import * as React from "react";
-import { RegisterView } from "./ui/RegisterView";
 import { RegisterController } from "@air/controller";
+
+import { RegisterView } from "./ui/RegisterView";
 
 // container -> view
 // container -> connector -> view
 // controller -> connector -> view
 
 export class RegisterConnector extends React.PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
-
   render() {
     return (
-      <React.Fragment>
-        <RegisterView submit={this.dummySubmit} />;
-        <RegisterController />;
-      </React.Fragment>
+      <RegisterController>
+        {({ submit }) => <RegisterView submit={submit} />}
+      </RegisterController>
     );
   }
 }
