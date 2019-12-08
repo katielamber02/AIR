cdc30c7f3e33b05ad3245cf7a55bcef20b80cb88
Katie Lamber <katielamber02@gmail.com>
2019-12-08 15:22:28 +0200

    types generated in controller package

---
+++ packages/controller/src/modules/RegisterController/__generated__/Register.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: Register
+// ====================================================
+
+export interface Register_register {
+  __typename: "Error";
+  path: string;
+  message: string;
+}
+
+export interface Register {
+  register: Register_register[] | null;
+}
+
+export interface RegisterVariables {
+  email: string;
+  password: string;
+}
--- packages/controller/src/modules/RegisterController/index.tsx
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -1,14 +1,82 @@
+// import * as React from "react";
+// import { graphql, ChildMutateProps } from "react-apollo";
+// import gql from "graphql-tag";
+
+// interface Props {
+//   children: (data: {
+//     submit: (values: any) => Promise<null>;
+//   }) => JSX.Element | null;
+// }
+
+// class C extends React.PureComponent<ChildMutateProps<Props, any, any>> {
+//   submit = async (values: any) => {
+//     console.log(values);
+//     const response = await this.props.mutate({
+//       variables: values
+//     });
+//     console.log("response: ", response);
+//     return null;
+//   };
+
+//   render() {
+//     return this.props.children({ submit: this.submit });
+//   }
+// }
+
+// const registerMutation = gql`
+//   mutation($email: String!, $password: String!) {
+//     register(email: $email, password: $password) {
+//       path
+//       message
+//     }
+//   }
+// `;
+
+// export const RegisterController = graphql(registerMutation)(C );
+
+// ---------2-------
+// import * as React from "react";
+
+// interface Props {
+//   children: (data: {
+//     submit: (values: any) => Promise<null>;
+//   }) => JSX.Element | null;
+// }
+
+// export class RegisterController extends React.PureComponent<Props> {
+//   submit = async (values: any) => {
+//     console.log(values);
+//     return null;
+//   };
+
+//   render() {
+//     return this.props.children({ submit: this.submit });
+//   }
+// }
+
+//   ---1----
 import * as React from "react";
+import { graphql, ChildMutateProps } from "react-apollo";
+import { Register, RegisterVariables } from "./__generated__/Register";
+
+import gql from "graphql-tag";
 
 interface Props {
   children: (data: {
-    submit: (values: any) => Promise<null>;
+    submit: (values: RegisterVariables) => Promise<null>;
   }) => JSX.Element | null;
 }
 
-export class RegisterController extends React.PureComponent<Props> {
-  submit = async (values: any) => {
+class C extends React.PureComponent<
+  ChildMutateProps<Props, Register, RegisterVariables>
+> {
+  submit = async (values: RegisterVariables) => {
     console.log(values);
+    const response = await this.props.mutate({
+      variables: values
+    });
+
+    console.log("response: ", response);
     return null;
   };
 
@@ -16,3 +84,23 @@ export class RegisterController extends React.PureComponent<Props> {
     return this.props.children({ submit: this.submit });
   }
 }
+const registerMutation = gql`
+  mutation Register($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      path
+      message
+    }
+  }
+`;
+// export const RegisterController = graphql(registerMutation)(C);
+
+// export const RegisterController = graphql<ChildMutateProps<Props, any, any>>(
+//   registerMutation
+// )(C);
+
+export const RegisterController = graphql<
+  Props,
+  Register,
+  RegisterVariables,
+  any
+>(registerMutation)(C);
